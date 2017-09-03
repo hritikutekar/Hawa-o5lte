1f66a0fc35cc3fe7653e27611fbb5e497458b3b6
hritik <hritikdrocks@gmail.com>
2017-09-03 08:02:39 +0000

    intial source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Sep 3 07:54:27 UTC 2017"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "hritik-on5-kernel-5239717"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
